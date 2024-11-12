9469a04f3956d9363f4910b1abb827a3e908c21f
R4huL-Srk <122245647+R4huL-Srk@users.noreply.github.com>
2024-11-12 23:44:24 +0530

    Create 37. Reverse video.py

---
+++ 37. Reverse video.py
@@ -0,0 +1,14 @@
+import cv2
+video_path = "vehicle_video.mp4"
+cap = cv2.VideoCapture(video_path)
+
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+for frame_number in range(total_frames - 1, -1, -1):
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to retrieve frame.")
+        break
+    cv2.imshow('Reverse Video', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
